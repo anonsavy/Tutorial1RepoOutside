09f0e6295bc6e4e8a3177d29bce498c3cdf3d3a9
anonsavy <89607921+anonsavy@users.noreply.github.com>
2021-09-20 19:48:23 -0400

    Add files via upload

--- dig3480-f21-t1-roll_a_ball-anonsavy-submission-1/Assets/Scripts/PlayerController.cs
+++ dig3480-f21-t1-roll_a_ball-anonsavy-submission-1/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,15 @@ public class PlayerController : MonoBehaviour
     public float speed = 0;
     public Text countText; 
     public Text winText;
+    public Text livesText;
+
+    public GameObject Player; 
 
     private Rigidbody rb;
     private float movementX;
     private float movementY;
     private int count; 
+    private int lives; 
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +26,12 @@ public class PlayerController : MonoBehaviour
         count = 0;
         SetCountText (); 
         winText.text = "";
+        lives = 3;
+        SetCountText ();
+        winText.text = "";
+        
+        
+        
     }
 
     void OnMove(InputValue movementValue)
@@ -47,14 +57,46 @@ public class PlayerController : MonoBehaviour
             count = count + 1;
             SetCountText (); 
         }
+        if (other.gameObject.CompareTag("Danger"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetCountText (); 
+        }
     }
 
     void SetCountText ()
     {
-        countText.text = "Count: " + count.ToString (); 
-        if (count >= 10)
+        countText.text = "Score: " + count.ToString (); 
+        if (count == 46)
         {
-            winText.text = "You Win! <3 Samuel Vera"; 
+            winText.text = "Victory! -Samuel Vera"; 
+        }
+
+        if (count == 28)
+        {
+            transform.position = new Vector3(0.0f, 60.5f, 0.0f); 
+        }
+
+        livesText.text = "Life: " + lives.ToString (); 
+        if (lives == 0)
+        {
+            winText.text = "Game Over"; 
+           // Player.gameObject.SetActive(false); 
+         //   else
+           // Player.gameObject.SetActive(true); 
+            
+        }
+
+        if (lives == 0)
+        {
+            Player.gameObject.SetActive(false); 
+        }
+        else
+        {
+             Player.gameObject.SetActive(true); 
         }
     }
+
+
 }
